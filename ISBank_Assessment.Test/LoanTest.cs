17b0fdfe605a2a848c28327a0d5a16e5081efdc6
Sithembiso Goqo <75358219+sithembisopgoqo@users.noreply.github.com>
2023-10-20 08:52:59 +0200

    Add files via upload

---
+++ ISBank_Assessment.Test/LoanTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ISBank_Assessment.DL;
+using ISBank_Assessment.BL;
+using ISBank_Assessment.BE;
+
+namespace ISBank_Assessment.Test
+{
+    [TestClass]
+    public class LoanTest
+    {
+
+        [TestMethod]
+        public void CalcLoanPaymentAmount()
+        {
+            var unitOfWork = new UnitOfWork(new DbContextFactory(null));
+
+            LoanLogic loanLogic = new LoanLogic(unitOfWork);
+
+            var loan = loanLogic.CalculateLoanPaymentAmount(5000,15,1);
+
+        }
+    }
+}
